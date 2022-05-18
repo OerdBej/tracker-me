fba7b23cc870782f790befcb7180d5e92779683b
oerd bej <oerdbej@gmail.com>
2022-05-18 16:20:23 +0200

    100%

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense /> 
       <Expenses items={expenses} />
 
     </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-
   const [title, setTitle] = useState(props.title);
-  
+
   const clickHandler = () => {
-    setTitle('Updated')
+    setTitle("Updated");
   };
 
   return (
