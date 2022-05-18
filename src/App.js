576895d38dfb6efb68e49aca998dbb040b0fe04b
oerd bej <oerdbej@gmail.com>
2022-05-18 16:17:48 +0200

    Form Working 100% - without user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expenses from './components/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 
 
@@ -29,8 +30,9 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
+
     </div>
   );
 }
