99af3992a315b4cad7b48827570be08cef05afa0
oerd bej <oerdbej@gmail.com>
2022-06-23 12:14:16 +0200

    cleared out

--- src/App.js
+++ src/App.js
@@ -1,51 +1,5 @@
-// this was the first edition of the app.js
-
-import React, { useState } from "react";
-import Expenses from "./components/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
-
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Book",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: "e3",
-    title: "Bicycle",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "My Car",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
-
 const App = () => {
-  // the value from data, grabbed in state.
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-  // to see if the data arrived here. this is trigered when new expense is put there.
-  const addExpenseHandler = (expense) => {
-    // so we use a function with all the data form expense to use it as paramater for the setexpense state in order to update the previus state with the latest data. dynamic list that get updated
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-  };
-
-  return (
-    <div>
-      {/* The same thing, passing as a props a function to new Expense. from app to newexpense and call it */}
-      <NewExpense onAddExpense={addExpenseHandler} />
-      {/* Dynamic list with added data here. */}
-      <Expenses items={expenses} />
-    </div>
-  );
+  return <h3>Hey</h3>;
 };
 
 export default App;
