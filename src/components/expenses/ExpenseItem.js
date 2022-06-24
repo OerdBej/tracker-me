18ad2a5afa4b84f7566197abf5087a77277b54fc
oerd bej <oerdbej@gmail.com>
2022-06-24 14:29:41 +0200

    46 making the structure of the components and assigning a Card wrapper

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,17 +1,18 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "../UI/Card";
 
 // todo multiple props. Getting props from app.js passing to this component and passing to expense component
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">$250</div>
       </div>
-    </div>
+    </Card>
   );
-}
+};
 
 export default ExpenseItem;
