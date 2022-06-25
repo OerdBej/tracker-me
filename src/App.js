96cbd5c0f130072b92bea6fe76467f3629ad9da1
oerd bej <oerdbej@gmail.com>
2022-06-25 09:06:57 +0200

    52 adding the form and the css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import Expenses from "./components/expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
+      title: "Books",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "Laptop", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
-      title: "Car Insurance",
+      title: "Bike",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
+      title: "New Chair",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
@@ -25,6 +26,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
